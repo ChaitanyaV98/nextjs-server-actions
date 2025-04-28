9203c4907d21a2cfdac5fd502ba1ae0e414311da
ChaitanyaV98 <chaitanya@betalectic.com>
2025-04-28 18:30:20 +0530

    added ui for get all users

--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -1,9 +1,26 @@
+import { fetchUsersAction } from "@/actions";
 import AddNewUser from "@/components/add-new-user";
-function UserManagement() {
+import SingleUserCard from "@/components/single-user-card";
+async function UserManagement() {
+  const getListOfUsers = await fetchUsersAction();
+  console.log("List of Usersss---", getListOfUsers);
   return (
-    <div className="P-20 max-w-6xl ">
-      <h1>User Management page</h1>
-      <AddNewUser />
+    <div className="p-20">
+      <div className="flex justify-between">
+        <h1>User Management</h1>
+        <AddNewUser />
+      </div>
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {getListOfUsers &&
+        getListOfUsers.data &&
+        getListOfUsers.data.length > 0 ? (
+          getListOfUsers.data.map((userItem, index) => (
+            <SingleUserCard user={userItem} key={index} />
+          ))
+        ) : (
+          <h3>No users found! Please create one</h3>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/single-user-card/index.js
@@ -0,0 +1,32 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+function SingleUserCard({ user }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          {user?.firstName} {user?.lastName}
+        </CardTitle>
+        <CardDescription>{user?.email}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{user.address}</p>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+export default SingleUserCard;
